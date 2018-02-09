21b405a12a6cf16a8bdc872182ad9f190da077b7
balodi <balodi@gmail.com>
2018-02-09 14:07:08 +0530

    added button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+class Button extends React.Component {
+  render()
+  {
+    return(
+<button class="contactme">
+contact me
+</button>
+    );
+  }
+}
 class Image extends React.Component {
   render()
   {
     return(
-<div class="image" >
+<div class="image" id="one" >
+<Button/>
 </div>
     );
   }
